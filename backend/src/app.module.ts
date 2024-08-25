ec3aba6b20020ec2359d34fba51d7f8dc0c0de93
Kormos03 <aronkormos@gmail.com>
2024-08-25 20:45:08 +0200

    userDto and postDto created, and first post created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
