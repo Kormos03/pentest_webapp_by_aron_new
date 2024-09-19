99c59f92c857e478268c7e4954a23dde427c979b
Kormos03 <aronkormos@gmail.com>
2024-09-19 08:46:58 +0200

    fake bank frontend moved to the frontend dircectory

---
+++ fake_bank/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
